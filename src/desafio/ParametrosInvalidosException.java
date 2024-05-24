40fa2d135b897edc92c8678a543074d7e503317b
marcosrogerio-jrf <marcosrogerio.jrf@gmail.com>
2024-05-24 08:42:40 -0400

    Desafio Controle de Fluxo

---
+++ src/desafio/ParametrosInvalidosException.java
@@ -0,0 +1,10 @@
+package desafio;
+
+public class ParametrosInvalidosException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ParametrosInvalidosException(String message) {
+		super(message);
+	}
+
+}
